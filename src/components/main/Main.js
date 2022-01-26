683f36a1401095ca3061d489aafd7514a513fd8c
deepakchethan <deepakchethan@outlook.com>
2022-01-26 21:07:43 +0530

    Fix: Reorder the form fields

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -23,17 +23,17 @@ export class Main extends React.Component {
         return (
           <div className="container">
               <div className="row">
-                  <div className="column ssh">
-                      <div className='input-container'>
-                        <label htmlFor="exampleFormControlInput1" className="form-label">SSH</label>
-                        <input type="text" className="form-input" id="exampleFormControlInput1" placeholder="git@github.com:giturls/giturls-app.git"/>
-                      </div>
-                  </div>
                   <div className="column http">
                       <div className='input-container'>
                         <label htmlFor="exampleFormControlInput2" className="form-label">HTTP</label>
                         <input type="text" className="form-input" id="exampleFormControlInput2" placeholder="https://github.com/giturls/giturls-app.git"/>
                       </div>
+                  </div>
+                  <div className="column ssh">
+                    <div className='input-container'>
+                      <label htmlFor="exampleFormControlInput1" className="form-label">SSH</label>
+                      <input type="text" className="form-input" id="exampleFormControlInput1" placeholder="git@github.com:giturls/giturls-app.git"/>
+                    </div>
                   </div>
               </div>
           </div>
